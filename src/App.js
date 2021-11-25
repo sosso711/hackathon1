ba3f9d1eebb17ba72bc00b544b8aa3fac19cef81
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-25 15:12:29 +0100

    Modified Profil

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Profil from "./Pages/Profil";
-import 
-import Navbar from "./components/Navbar";
+
+
 
 import Footer from "./components/Footer";
 import "./App.css";
