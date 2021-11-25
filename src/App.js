6a1ad0938fd3d29e1c61705cd3e02b11fc195c86
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-25 15:23:28 +0100

    Add contact and UserForm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Profil from "./Pages/Profil";
-
+import Contact from "./Pages/Contact";
+import UserForm from "./Pages/UserForm"
 
 
 import Footer from "./components/Footer";
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profil" element={<Profil />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/userForm" element={<Home />} />
+        <Route path="/userForm" element={<UserForm/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
