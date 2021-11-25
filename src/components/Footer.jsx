e5681a3d03c006fd5e9011f1b10461084d58fb6c
bahstiien <bastien.lecalvez@gmail.com>
2021-11-25 15:47:04 +0100

    update css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,17 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center bg-blue-700 w-full h-24">
-      <div className="px-10">
-        <h2 className="text-gray-200"> Social Media</h2>
+    <div className="flex-col flex justify-around items-center bg-blue-700 w-full h-auto sm:flex-row">
+      <div className="flex items-center justify-center m-10">
+        <p className="text-gray-200 mx-4 text-lg  ">Login</p>
+        <AccountCircleIcon />
+      </div>
+      <div className="px-10 p-10">
+        <h2 className="text-gray-200 text-lg mb-2"> Social Media</h2>
         <FacebookIcon />
         <TwitterIcon />
         <InstagramIcon />
