65fb7bb9d3d1435748db109241f77aef4d22b82c
bahstiien <bastien.lecalvez@gmail.com>
2021-11-25 13:59:32 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
+
+import Footer from "./components/Footer";
+import "./App.css";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profil" element={<Home />} />
+        <Route path="/contact" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
