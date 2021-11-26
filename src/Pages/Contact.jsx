ce5c996248dedda62d08c17e4892fd3c01fe0c5a
bahstiien <bastien.lecalvez@gmail.com>
2021-11-26 10:15:36 +0100

    add slide

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import FakeUser from "../Assets/fakeuser.png";
-import VerticalCarousel from "../components/VerticalCarousel";
+import Slide from "../components/Slide";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div>
       <Header name="My contacts" />
-      <div className="flex justify-center items-center">
-        <img
-          src={FakeUser}
-          alt="fake user"
-          className="rounded-full shadow-lg m-6 w-48"
-        />
-      </div>
-      <VerticalCarousel />
-      <div className="border-2 border-gray-500 w-2/5 text-center rounded-xl flex justify-center flex-col">
-        <p>Bill Sanders</p>
-        <p>bill.sanders@wild.com</p>
-        <p>Maanger de tournée</p>
-        <p>+33 12345678</p>
+
+      <div className="flex justify-center m-10">
+        <Slide />
       </div>
     </div>
   );
